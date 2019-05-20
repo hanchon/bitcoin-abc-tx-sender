4cd945d18603ca85cb8e846b449edc543b864bdd
Guillermo <guillermo.paoletti@gmail.com>
2019-05-20 22:38:16 +0200

    Removed old code

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 var cfg = require('../config.json');
 var RPC = require('./rpc.js');
-var Wallet = require('./wallet.js')
-var bitcore = require('bitcore-lib-cash');
 
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
 var port = 24004;
 
 
 const rpc = new RPC(cfg.rpcip, cfg.rpcport, cfg.rpcuser, cfg.rpcpassword);
 
 //Start the server
-server.listen(port, async ()=>{
+app.listen(port, async ()=>{
     await rpc.mineBlocks(1);
     return "A block was mined";
 });
